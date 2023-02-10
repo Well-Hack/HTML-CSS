628e8467dba63d28288de8de32d071a66b573ade
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-10 17:57:43 -0300

    att
    att

---
+++ Exercicios/Javascript/exerc05.js
@@ -0,0 +1,9 @@
+var idade = 12
+
+if(idade < 16){
+    console.log('Você não vota')
+} else if(idade >= 16 && idade < 18){
+    console.log('Voto opcional')
+}else{
+    console.log('Voto obrigatorio')
+}
