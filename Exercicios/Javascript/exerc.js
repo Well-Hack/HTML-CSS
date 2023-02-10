ab3dff077ce26e26f7e201258a142a8796468c7f
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-10 16:45:48 -0300

    dentra
    criacao de logica de multar acima do limite de velocidade

---
+++ Exercicios/Javascript/exerc.js
@@ -0,0 +1,6 @@
+var vel = 72
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+    if (vel >= 80){
+        console.log('Você ultrapassou a velocidade permitida, Multado')}
+console.log("Dirija com segurança")
+
---
+++ Exercicios/Javascript/exerc02.js
@@ -0,0 +1,8 @@
+var pais = "Brasil"
+
+    if (pais == "Brasil"){
+        console.log("Ele é brasileiro")
+    }
+    else{
+        console.log("Voce não é brasileiro")
+    }
