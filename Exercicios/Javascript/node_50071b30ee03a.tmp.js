e1830dd5cca7e79c9c4fadb5a2bd5eafbe2287b4
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-10 16:17:35 -0300

    att
    att

---
+++ Exercicios/Javascript/node_50071b30ee03a.tmp.js
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somando Valores</title>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+        }
+        body{
+            text-align: center;
+            vertical-align: center;
+            background-color: yellow;
+        }
+        h1{
+            font-size: 18pt;
+        }
+
+        #res{
+            text-align: center;
+        }
+        input{
+            vertical-align: center;
+        }
+
+        input{
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Somando Valores</h1>
+    <input type="number" name="txtn1" id="txtn1">
+    <input type="number" name="txtn2" id="txtn2">
+    <input type="button" value="Somar" onclick="somar()">
+    <div id="res">Resultado</div>
+    <script>
+        function somar(){
+            var tn1 = document.getElementById('txtn1')
+            var tn2 = document.getElementById('txtn2')
+            var som = document.getElementById('res')
+            var n1 = Number(tn1.value)
+            var n2 = Number(tn2.value)
+            var s1 = n1 + n2
+            som.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${s1}`
+        }
+    </script>
+</body>
+</html>
