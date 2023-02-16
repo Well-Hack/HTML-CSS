e4e3acfa1aa40d862087a40c2456f3f1d802bf10
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-16 18:06:27 -0300

    javascript
    exercicio concluido, hora do dia

--- Exercicios/Javascript Exerc01/script.js
+++ Exercicios/Javascript Exerc01/script.js
@@ -1,7 +1,26 @@
-function carregar(){
-var msg = document.getElementById("msg")
-var img = document.getElementById("imagem")
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Àgora são ${hora} horas`
+function carregar() {
+    var msgm = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+
+    msgm.innerHTML = `Àgora são ${hora} horas e ${min} Minutos`
+
+    if (hora >= 0 && hora < 12){
+
+        img.src = "Imagens/manhãcity.jpg"
+        document.body.style.background = "#A5DDF0"
+
+    }else if(hora >= 12 && hora < 18){
+
+        img.src = "Imagens/sunset.jpg"
+        document.body.style.background = "#F0A250"
+
+    }else{
+        img.src = "Imagens/noitecity.jpg"
+        document.body.style.background = "#0012DB"
+        
+    }
+
 }
