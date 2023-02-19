5fb4dfa07fe98ad7921aee9f2360aba898f3ace6
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-19 15:13:04 -0300

    while
    estudando while

---
+++ Exercicios/Javascript/Javascriptwhile.js/while.js
@@ -0,0 +1,16 @@
+var cont = 1
+ while (cont <= 5){
+     console.log(`passo ${cont}`)
+     cont++
+ }
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
