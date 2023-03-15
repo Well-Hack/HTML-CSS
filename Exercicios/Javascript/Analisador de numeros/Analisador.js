75202059f0552d8b0853a3963f66e07edb8daf6d
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-15 18:15:40 -0300

    add
    ada

--- Exercicios/Javascript/Analisador de numeros/Analisador.js
+++ Exercicios/Javascript/Analisador de numeros/Analisador.js
@@ -1,6 +1,28 @@
 let num = document.querySelector('input#num')
+let list = document.querySelector('select#tabela')
+let res = document.querySelector('div#res')
+let valor = []
 
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
+    }
+}
 
-function adicionar(){
+function inList(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }    
+}
 
+function add(){
+    if(isNumero(num.value) && !inList(num.value, valor)){
+        window.alert('Tudo OK!')
+    }else{
+        window.alert('Valor invalido ou já tem na lista')
+    }
 }
