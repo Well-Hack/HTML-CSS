e18cdb72865635c2f80653f033e4c0709d52b47d
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-15 18:40:20 -0300

    javascript

--- Exercicios/Javascript/Analisador de numeros/Analisador.js
+++ Exercicios/Javascript/Analisador de numeros/Analisador.js
@@ -21,8 +21,30 @@ function inList(n, l){
 
 function add(){
     if(isNumero(num.value) && !inList(num.value, valor)){
-        window.alert('Tudo OK!')
+       valor.push(Number(num.value))
+       let item = document.createElement("option")
+       item.text = `Valor ${num.value} adicionado`
+       list.appendChild(item)
+       res.innerHTML=``
     }else{
         window.alert('Valor invalido ou já tem na lista')
+    }
+    num.value = ""
+    num.focus()
+}
+
+function end(){
+    if(valor.length == 0){
+    window.alert('Adicione Algum valor')
+    } else {
+        let tot = valor.length
+        let mai = valor[0]
+        let men = valor[0]
+        for(let pos in valor){
+            
+        }
+        
+        res.innerHTML =  ``
+        res.innerHTML = `<p> Ao todo temos ${tot} números cadastrados. </p>`
     }
 }
